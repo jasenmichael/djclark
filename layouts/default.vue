9c5152e4c46533917ee7a44926647ce95ff039d6
jasenmichael <jasen@ready-builder.com>
2021-03-13 12:28:41 -0600

    chore: setup, create page

---
+++ layouts/default.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="bg-gray-900 text-white">
+    <div class="flex flex-col min-h-screen">
+      <!-- <Nav /> -->
+      <transition name="page" mode="in-out">
+        <!-- <Breadcrumbs
+          v-if="showOn.some((el) => $route.path.includes(el))"
+          class="pt-16 lg:pt-20"
+        /> -->
+      </transition>
+      <Nuxt class="flex-grow" />
+      <!-- <Footer /> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      // showOn: ['/photos', '/blog', '/projects'],
+    }
+  },
+  head() {
+    return {
+      // script: [
+      //   { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
+      // ],
+    }
+  },
+}
+</script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.25s ease-in-out, transform 0.25s ease-in-out;
+
+  /* transition: opacity 0.5s; */
+}
+.page-enter,
+.page-leave-active {
+  opacity: 0;
+  transform: translate3d(0, 15px, 0);
+}
+
+/* .fade-in-enter-active {
+  transition: all 0.8s ease;
+}
+
+.fade-in-leave-active {
+  opacity: 0;
+  transform: translate3d(0, 15px, 0);
+}
+
+.fade-in-enter,
+.fade-in-leave-to {
+  position: absolute;
+  opacity: 0;
+} */
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>
+      <img src="/dj-clarky.jpeg" alt="Dj Clark Orwick" />
+    </div>
+    <h1 class="text-2xl">DJ Clark Orwick</h1>
+    <h2 class="text-xl">Playlists:</h2>
+    <ul>
+      <li v-for="(pl, i) in playlists" :key="i">
+        <a :href="pl" class="text-yellow-100">
+          {{ pl.replace('/clark/', '').replace('.m3u', '') }}
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      playlists: [
+        '/clark/Dance.m3u',
+        '/clark/NewFavs.m3u',
+        '/clark/Sex.m3u',
+        '/clark/SlowBoil.m3u',
+        '/clark/SlowDance.m3u',
+        '/clark/Tonight.m3u',
+      ],
+    }
+  },
+}
+</script>
